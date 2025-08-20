60e5314dc094ef17ae81a0f3f9264876da271269
Anshul Yadav <anshul333y@gmail.com>
2025-08-20 23:22:23 +0530

    removed pywalfox

--- hyprland_install.sh
+++ hyprland_install.sh
@@ -48,8 +48,7 @@ git clone https://aur.archlinux.org/paru-bin.git ~/dl/paru
 cd ~/dl/paru && makepkg -si --noconfirm && cd && rm -rf ~/dl/paru
 
 # installing aur packages
-paru -S --noconfirm hyprshot-git python-pywalfox google-chrome \
-  visual-studio-code-bin
+paru -S --noconfirm hyprshot-git google-chrome visual-studio-code-bin
 
 # post install steps
 rm .bash* .zshrc
